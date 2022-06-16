9a9fda99ab38367d96bde1fe4aac7953b5483099
Oriol Trujillo <trujillo.oriol@gmail.com>
2022-06-16 15:39:55 +0200

    Github actions workflow & Terrafrome: changes input vars.

--- main.tf
+++ main.tf
@@ -13,6 +13,15 @@ terraform {
     }
   }
 }
+
+variable "AWS_ACCESS_KEY_ID" {
+  type = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type = string
+}
+
 variable "AWS_REGION" {
   type = string
 }
@@ -30,6 +39,9 @@ variable "AWS_ECR_REPO" {
 }
 
 provider "aws" {
+  region     = var.AWS_REGION
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
 data "aws_caller_identity" "current" {}
