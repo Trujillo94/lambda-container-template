9ef61e81fa4f7024e022a6d290655cafe1fb73c0
Oriol Trujillo <trujillo.oriol@gmail.com>
2022-06-17 16:20:54 +0200

    Transforming proxy integration to regular.

--- main.tf
+++ main.tf
@@ -158,13 +158,13 @@ resource "aws_api_gateway_rest_api" "sample_api" {
 resource "aws_api_gateway_resource" "sample_resource" {
   rest_api_id = aws_api_gateway_rest_api.sample_api.id
   parent_id   = aws_api_gateway_rest_api.sample_api.root_resource_id
-  path_part   = "{proxy+}"
+  path_part   = "sample_resource"
 }
 
 resource "aws_api_gateway_method" "sample_method" {
   rest_api_id   = aws_api_gateway_rest_api.sample_api.id
   resource_id   = aws_api_gateway_resource.sample_resource.id
-  http_method   = "ANY"
+  http_method   = "POST"
   authorization = "NONE"
 }
 
@@ -174,7 +174,7 @@ resource "aws_api_gateway_integration" "sample_integration" {
   http_method = aws_api_gateway_method.sample_method.http_method
 
   integration_http_method = "POST"
-  type                    = "AWS_PROXY"
+  type                    = "AWS"
   uri                     = aws_lambda_function.sample_lambda.invoke_arn
   timeout_milliseconds    = 29000
 
@@ -192,20 +192,20 @@ resource "aws_api_gateway_integration" "sample_integration" {
   }
 }
 
-resource "aws_api_gateway_method" "proxy_root" {
+resource "aws_api_gateway_method" "method_root" {
   rest_api_id   = aws_api_gateway_rest_api.sample_api.id
   resource_id   = aws_api_gateway_rest_api.sample_api.root_resource_id
-  http_method   = "ANY"
+  http_method   = "POST"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "lambda_root" {
   rest_api_id = aws_api_gateway_rest_api.sample_api.id
-  resource_id = aws_api_gateway_method.proxy_root.resource_id
-  http_method = aws_api_gateway_method.proxy_root.http_method
+  resource_id = aws_api_gateway_method.method_root.resource_id
+  http_method = aws_api_gateway_method.method_root.http_method
 
   integration_http_method = "POST"
-  type                    = "AWS_PROXY"
+  type                    = "AWS"
   uri                     = aws_lambda_function.sample_lambda.invoke_arn
 }
 
