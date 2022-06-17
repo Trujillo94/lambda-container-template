8f6373870e465dc78252a7241cc39388948c27d7
Oriol Trujillo <trujillo.oriol@gmail.com>
2022-06-17 15:03:27 +0200

    Fixing API resource naming.

--- main.tf
+++ main.tf
@@ -158,7 +158,7 @@ resource "aws_api_gateway_rest_api" "sample_api" {
 resource "aws_api_gateway_resource" "sample_resource" {
   rest_api_id = aws_api_gateway_rest_api.sample_api.id
   parent_id   = aws_api_gateway_rest_api.sample_api.root_resource_id
-  path_part   = "/"
+  path_part   = "sample_resource"
 }
 
 resource "aws_api_gateway_method" "sample_method" {
