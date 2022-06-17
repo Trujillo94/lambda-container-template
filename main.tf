d59a545229131e53f7983de85447c90c192d89c7
Oriol Trujillo <trujillo.oriol@gmail.com>
2022-06-17 16:39:07 +0200

    Fixing API Gateway integration response.

--- main.tf
+++ main.tf
@@ -182,30 +182,6 @@ resource "aws_api_gateway_integration" "sample_integration" {
     "integration.request.header.X-Authorization" = "'static'"
   }
 
-resource "aws_api_gateway_method_response" "response_200" {
-  rest_api_id = aws_api_gateway_rest_api.sample_api.id
-  resource_id = aws_api_gateway_resource.sample_resource.id
-  http_method = aws_api_gateway_method.sample_method.http_method
-  status_code = "200"
-}
-
-  resource "aws_api_gateway_integration_response" "MyDemoIntegrationResponse" {
-  rest_api_id = aws_api_gateway_rest_api.sample_api.id
-  resource_id = aws_api_gateway_resource.sample_resource.id
-  http_method = aws_api_gateway_method.sample_method.http_method
-  status_code = aws_api_gateway_method_response.response_200.status_code
-
-  # Transforms the backend JSON response to XML
-  response_templates = {
-    "application/xml" = <<EOF
-#set($inputRoot = $input.path('$'))
-<?xml version="1.0" encoding="UTF-8"?>
-<message>
-    $inputRoot.body
-</message>
-EOF
-  }
-
   # Transforms the incoming XML request to JSON
   request_templates = {
     "application/xml" = <<EOF
@@ -213,6 +189,32 @@ EOF
       "body" : $input.json('$')
     }
     EOF
+  }
+}
+
+resource "aws_api_gateway_method_response" "response_200" {
+  rest_api_id = aws_api_gateway_rest_api.sample_api.id
+  resource_id = aws_api_gateway_resource.sample_resource.id
+  http_method = aws_api_gateway_method.sample_method.http_method
+  status_code = "200"
+}
+
+resource "aws_api_gateway_integration_response" "MyDemoIntegrationResponse" {
+  rest_api_id = aws_api_gateway_rest_api.sample_api.id
+  resource_id = aws_api_gateway_resource.sample_resource.id
+  http_method = aws_api_gateway_method.sample_method.http_method
+  status_code = aws_api_gateway_method_response.response_200.status_code
+
+
+  # Transforms the backend JSON response to XML
+  response_templates = {
+    "application/xml" = <<EOF
+      #set($inputRoot = $input.path('$'))
+      <?xml version="1.0" encoding="UTF-8"?>
+      <message>
+          $inputRoot.body
+      </message>
+      EOF
   }
 }
 
